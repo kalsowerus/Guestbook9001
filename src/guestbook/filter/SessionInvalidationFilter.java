a1b8858af889ec94d6aa09158278a45a57ef4285
Roy Geesing <roy.geesing@hispeed.ch>
2017-09-25 14:02:29 +0200

    code cleanup

--- src/guestbook/filter/SessionInvalidationFilter.java
+++ src/guestbook/filter/SessionInvalidationFilter.java
@@ -4,36 +4,34 @@ import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
-import java.util.Date;
 
 public class SessionInvalidationFilter implements Filter {
-    @Override
-    public void init(FilterConfig filterConfig) throws ServletException {
+    public static final String LAST_ACTIVE_SESSION_ATTRIBUTE = "lastActive";
+    public static final int SESSION_TIMEOUT = 5 * 60 * 1000; // 5 minutes
 
-    }
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {}
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         HttpServletRequest request = (HttpServletRequest) servletRequest;
         HttpSession session = request.getSession(true);
-        Long lastActiveObject = (Long) session.getAttribute("lastActive");
+        Long lastActiveObject = (Long) session.getAttribute(LAST_ACTIVE_SESSION_ATTRIBUTE);
         long currentTime = System.currentTimeMillis();
 
         if(lastActiveObject != null) {
             long lastActive = lastActiveObject.longValue();
-            if(currentTime - lastActive > (5 * 60 * 1000)/*5 minutes*/) {
+            if(currentTime - lastActive > SESSION_TIMEOUT) {
                 session.invalidate();
                 session = request.getSession(true);
             }
         }
 
-        session.setAttribute("lastActive", Long.valueOf(currentTime));
+        session.setAttribute(LAST_ACTIVE_SESSION_ATTRIBUTE, Long.valueOf(currentTime));
 
         filterChain.doFilter(request, servletResponse);
     }
 
     @Override
-    public void destroy() {
-
-    }
+    public void destroy() {}
 }
