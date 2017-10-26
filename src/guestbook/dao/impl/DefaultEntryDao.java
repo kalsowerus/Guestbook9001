56578fc3bcfc90546fbfc3c292886628126e824e
k42m <kenny.maurer@bluewin.ch>
2017-10-26 17:27:14 +0200

    create entry

--- src/guestbook/dao/impl/DefaultEntryDao.java
+++ src/guestbook/dao/impl/DefaultEntryDao.java
@@ -3,13 +3,16 @@ package guestbook.dao.impl;
 import guestbook.dao.EntryDao;
 import guestbook.entity.Entry;
 import guestbook.helper.EntityManagerHelper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
+import javax.persistence.EntityManager;
 import java.util.List;
 
 //@Component
 public class DefaultEntryDao implements EntryDao{
-
+    private static final Logger LOG = LoggerFactory.getLogger(DefaultEntryDao.class);
     private final EntityManagerHelper emh = new EntityManagerHelper();
 
     @Override
@@ -24,7 +27,14 @@ public class DefaultEntryDao implements EntryDao{
 
     @Override
     public void createEntry(Entry entry) {
-
+        EntityManager em = EntityManagerHelper.getEntityManager();
+        EntityManagerHelper.beginTransaction();
+        try {
+            em.persist(entry);
+        } catch (Exception e) {
+            LOG.error("Error while running query: " + e.getMessage());
+        }
+        EntityManagerHelper.commitAndCloseTransaction();
     }
 
     @Override
